cd0f77c2cd3f494387a3ede85b219c3156aeb42a
xueweihan <595666367@qq.com>
2015-11-24 10:03:22 +0800

    learn how to use the django templet

--- test_site/test_site/urls.py
+++ test_site/test_site/urls.py
@@ -1,9 +1,14 @@
+#coding:utf-8
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'test_web_app.views.index', name='home'),
+    url(r'^$', 'test_web_app.views.index', name='index'),
+    url(r'^home$', 'test_web_app.views.home', name='home'),
+    url(r'^add/$', 'test_web_app.views.add', name='add'),
+    url(r'^add/(\d+)/(\d+)/$', 'test_web_app.views.add2', name='add2'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- test_site/test_web_app/views.py
+++ test_site/test_web_app/views.py
@@ -1,5 +1,40 @@
 #coding:utf-8
+
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse(u'Hello xueweihan!!')
+
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+
+def home(request):
+    # return render(request, 'home.html')
+
+    # 加上模版引擎
+    test_string = '这是传递过来的字符串！'
+
+    # 数组
+    test_list = [u'香蕉', u'苹果', u'西瓜', u'嘿嘿']
+    test_dict = {'Hero': u'蝙蝠侠盗',
+                 'Villain': u'采花大盗'}
+
+    # 用于练习逻辑判断的变量var
+    var = 88
+    return render(request,
+                  'home.html',
+                  {'test_list': test_list,
+                   'test_string': test_string,
+                   'test_dict': test_dict,
+                   'var':var})
